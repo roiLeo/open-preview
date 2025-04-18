8715b89ea85f23e3ae5ac63756916902564d31d6
roiLeo <medina.leo42@gmail.com>
2025-04-18 16:07:22 +0200

    🛳️ Nuxt UI v3 migration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,6 @@ export default defineNuxtConfig({
       title: 'OpenPreview',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      bodyAttrs: { class: 'bg-white dark:bg-neutral-900' }
     }
   },
 
